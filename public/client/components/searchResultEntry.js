5abe69798b252309031166579a76e9cdef0ec534
Hank <hbowen1123@gmail.com>
2017-06-30 14:55:17 -0500

    Moved API queries for tv and movie entry details out of tv and movie entry components and into searchResultEntry and app.js. Seems to fix rendering bugs.

--- public/client/components/searchResultEntry.js
+++ public/client/components/searchResultEntry.js
@@ -1,9 +1,10 @@
 angular.module('main-app') // copied mostly from ng-cast
 
-.controller('SearchCtrl2', function($http, searchTheMovieDB) {
+.controller('SearchCtrl2', function($http, searchTheMovieDB, searchOMDB) {
   this.imdb_id;
   this.$onInit = function() {
-    this.TMDBservice = searchTheMovieDB
+    this.TMDBservice = searchTheMovieDB;
+    this.OMDBService = searchOMDB;
     
     this.handleMovieClick = function() {
       this.TMDBservice.searchById(this.result.id, this.selection, (data) => {
@@ -14,20 +15,30 @@ angular.module('main-app') // copied mostly from ng-cast
           $http.post('/addMovie', {user: this.user.username, imdb_id: this.imdb_id}).then(() => {
             $http.get('/sess').then((session) => {
               this.user.movies = session.data.movies;
+            }).then(() => {
+              this.user.movies.forEach(movie => {
+                this.OMDBService.search({i: movie.imdb_id}, (data) => {
+                  movie.details = data;
+                  movie.details.Poster === "N/A" || !movie.details.Poster ? movie.details.Poster = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png' : movie.details.Poster;
+                })
+              })
             });
           });
         }
         else if (this.selection === 'tv') {
           $http.post('/addTv', {user: this.user.username, imdb_id: this.imdb_id}).then(() => {
             $http.get('/sess').then((session) => {
-              this.dude = session;
-              console.log('getting here user', this.user.tvShows)
-              console.log('getting here session', session)
-              this.user.tvShows = this.dude.data.tvShows;
-              console.log('then here user', this.user.tvShows)
-              console.log('then here session', this.dude.data.tvShows)
-            })
-          })
+              this.user.tvShows = session.data.tvShows;
+            }).then(() => {
+              this.user.tvShows.forEach(tvShow => {
+                this.TMDBservice.searchById(tvShow.imdb_id, 'tv', (data) => {
+                  tvShow.details = data;
+                  tvShow.poster = data.poster_path ? 'https://image.tmdb.org/t/p/w1280' + data.poster_path :
+                                                  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';
+                })
+              })
+            });
+          });
         }
       });
     };
@@ -38,7 +49,7 @@ angular.module('main-app') // copied mostly from ng-cast
   return {
     scope: {
       result: '<',
-      user: '<',
+      user: '=',
       selection: '<'
     },
     restrict: 'E',
--- public/client/components/tvEntry.js
+++ public/client/components/tvEntry.js
@@ -8,15 +8,6 @@ angular.module('main-app')
     },
     restrict: 'E',
     controller: function(searchTheMovieDB, $http) {
-      this.$onInit = function() {
-        this.TMDBService = searchTheMovieDB;
-        this.TMDBService.searchById(this.tvShow.imdb_id,'tv', (data) => {
-          console.log('tvShow data: ', data)
-          this.tvShow.details = data;
-          this.tvShow.poster = data.poster_path ? 'https://image.tmdb.org/t/p/w1280' + data.poster_path :
-                                                  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';
-        })
-      }
 
       this.handleAddCommentClick = function() {
         var idList = this.user.tvShows.map((x) => x.imdb_id);

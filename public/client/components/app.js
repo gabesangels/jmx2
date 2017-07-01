d58380e4b134debb6553f0d3ce8cad83ab8e3ea5
Hank <hbowen1123@gmail.com>
2017-06-30 19:08:45 -0500

    Movie and Tv details are now stored in the database. API calls for this data now occur only in the handleClick function in searchResultEntry.

--- public/client/components/app.js
+++ public/client/components/app.js
@@ -17,24 +17,6 @@ angular.module('main-app')
     this.user.username = this.intendedUser.data.username;
     this.user.movies = this.intendedUser.data.movies;
     this.user.tvShows = this.intendedUser.data.tvShows;
-
-    this.user.movies.forEach(item => {
-      this.OMDBService.search({i: item.imdb_id}, (data) => {
-        item.details = data;
-        if(item.details.Actors !== "N/A" && item.details.Actors) {
-          this.actors = item.details.Actors.split(', ')
-        }
-        item.details.Poster === "N/A" || !item.details.Poster ? item.details.Poster = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png' : item.details.Poster
-      })
-    });
-
-    this.user.tvShows.forEach(tvShow => {
-      this.TMDBservice.searchById(tvShow.imdb_id, 'tv', (data) => {
-        tvShow.details = data;
-        tvShow.poster = data.poster_path ? 'https://image.tmdb.org/t/p/w1280' + data.poster_path :
-                                        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';
-      })
-    })
   });
 })
 .directive('app', function() { // directive name is the HTML tag name REMEMBER THIS

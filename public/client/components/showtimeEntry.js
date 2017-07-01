6ffb36bf277cb8fb7bbd391364f9a99aaf2418f5
Hank <hbowen1123@gmail.com>
2017-07-01 15:30:56 -0500

    Changed date format on showtimeEntry.

--- public/client/components/showtimeEntry.js
+++ public/client/components/showtimeEntry.js
@@ -10,7 +10,22 @@ angular.module('main-app')
       query: '<',
     },
     restrict: 'E',
-    controller: 'HomeCtrl',
+    controller: function() {
+      this.$onInit = () => {
+        var d = new Date(this.showtime.dateTime);
+        this.dateTime = this.formatDate(d);
+      }
+      this.formatDate = (date) => {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+      }
+    },
     controllerAs: 'ctrl',
     bindToController: true,
     templateUrl: 'public/client/templates/showtimeEntry.html'
